0be9b1a3478a785836dbc0d8bd70094aca813dbe
nithya <nithukitty112@gmail.com>
2025-05-15 23:46:38 +0530

    cnages

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -10,7 +10,7 @@ export default function ResetPassword() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post(`https://backend-password-reset-1-8isk.onrender.com/api/auth/request-reset/${token}`, { password });
+      await axios.post(`https://backend-password-reset-1-8isk.onrender.com/api/auth/reset-password/${token}`, { password });
 
 
       alert('Password reset successfully');
